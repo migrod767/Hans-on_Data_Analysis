17f37025a60e6c4fcb7e06427fd745b99bc05725
Miguel <mf.rodriguez12@uniandes.edu.co>
2020-07-13 21:04:52 -0500

    Ch2. exercises

--- movie_lens.py
+++ movie_lens.py
@@ -26,6 +26,42 @@ def explore_data(users, ratings, movies):
     print(data.info())
     print('\n')
 
+    print(data.head(5))
+    print('\n')
+
+    #mean movie ratings for each film grouped by gender
+    mean_ratings = data.pivot_table('rating', index='title', columns='gender', aggfunc='mean')
+    print(mean_ratings)
+    print('\n')
+
+    ratings_by_title = data.groupby('title').size()
+    print(ratings_by_title[:10])
+    print('\n')
+
+    active_titles = ratings_by_title.index[ratings_by_title >= 250]
+    print(active_titles)
+    print('\n')
+
+    mean_ratings = mean_ratings.loc[active_titles]
+    print(mean_ratings)
+    print('\n')
+
+    #To see the top films among female viewers.
+    top_female_ratings = mean_ratings.sort_values(by=['F'], ascending=False)
+    print(top_female_ratings[:10])
+    print('\n')
+
+    #Calculate diferent in rating between F and M
+    mean_ratings['diff'] = mean_ratings['M'] - mean_ratings['F']
+    sorted_by_diff = mean_ratings.sort_values(by="diff", ascending=True)
+    print(sorted_by_diff[:10])
+    print('\n')
+
+    #disgreement in the ratings in the viewers
+    rating_std_by_title = data.groupby('title')['rating'].std()
+    rating_std_by_title = rating_std_by_title.loc[active_titles]
+    print(rating_std_by_title.sort_values(ascending=False)[:10])
+    print('\n')
 
 if __name__ == '__main__':
     users, ratings, movies = load_data()
